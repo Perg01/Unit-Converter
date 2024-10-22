dc4f57379756dfa1ac5994b0e8f4ff6332d59234
Perg01 <jsatshs@gmail.com>
2024-10-21 17:47:16 -0700

    Add logic to handle length conversions

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 function openTab(event, tabName) {
-    var i, tabcontent, tablinks;
+    let i, tabcontent, tablinks;
 
     tabcontent = document.getElementsByClassName('tabcontent');
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = 'none';
+        tabcontent[i].style.visibility = 'hidden';
+        tabcontent[i].style.opacity = '0';
+        tabcontent[i].style.position = 'absolute';
     }
 
     tablinks = document.getElementsByClassName('tablinks');
@@ -11,8 +13,37 @@ function openTab(event, tabName) {
         tablinks[i].className = tablinks[i].className.replace(' active', '');
     }
 
-    document.getElementById(tabName).style.display = 'block';
+    let selectedTab = document.getElementById(tabName);
+    selectedTab.style.visibility = 'visible';
+    selectedTab.style.opacity = '1';
+    selectedTab.style.position = 'relative';
+
     event.currentTarget.className += ' active';
 }
+
+function convertLength(value, fromUnit, toUnit) {
+    const units = {
+        millimeter: 0.001,
+        centimeter: 0.01,
+        meter: 1,
+        kilometer: 1000,
+        inch: 0.0254,
+        foot: 0.3048,
+        yard: 0.9144,
+        mile: 1609.34
+    };
+
+    const result = (value * units[fromUnit]) / units[toUnit];
+    return result.toFixed(1);
+}
+
+document.querySelector('#Length form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const value = document.querySelector('#length-value').value;
+    const fromUnit = document.querySelector('#from-unit-length').value;
+    const toUnit = document.querySelector('#to-unit-length').value;
+    const result = convertLength(value, fromUnit, toUnit);
+    document.querySelector('#Length .result').textContent = `Converted Value: ${result}`;
+});
 
 document.getElementsByClassName('tablinks')[0].click();
